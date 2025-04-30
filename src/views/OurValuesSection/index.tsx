c92ef5bb95178dbed83730eac4a89dccd481a9ee
il94 <ilyes.landolsi@outlook.fr>
2025-04-30 20:34:54 +0200

    update: add desktop display of OurValuesSection

--- src/views/OurValuesSection/index.tsx
+++ src/views/OurValuesSection/index.tsx
@@ -5,28 +5,30 @@ import Card from "./Card"
 
 export default function OurValuesSection() {
 	return (
-		<section className="flex w-full flex-col items-center justify-center gap-8 px-7 py-20">
-			<h2 className="font-nohemi text-primary text-center text-4xl font-semibold">
+		<section className="flex w-full flex-col items-center justify-center gap-8 px-7 py-20 lg:gap-15 lg:px-20 lg:py-40">
+			<h2 className="font-nohemi text-primary text-center text-4xl font-semibold lg:text-5xl">
 				Nos valeurs
 			</h2>
-			<Card
-				src={coffeIcon}
-				alt="coffe-icon"
-				title="Passioné de Café"
-				description="Notre équipe est passionnée par l'art de préparer le café. Chaque grain est soigneusement sélectionné pour offrir une expérience gustative exceptionnelle. Nous nous engageons à vous offrir des saveurs riches et uniques dans chaque tasse."
-			/>
-			<Card
-				src={catIcon}
-				alt="cat-icon"
-				title="Bien-être des Chats"
-				description="Le bonheur et le bien-être de nos amis félins sont au cœur de notre philosophie. Nous offrons un environnement sûr et confortable à nos chats résidents, où ils peuvent interagir avec nos clients et recevoir toute l'attention et l'affection qu'ils méritent."
-			/>
-			<Card
-				src={plantIcon}
-				alt="plant-icon"
-				title="Engagement Environnemental"
-				description="Nous sommes soucieux de l'impact environnemental de notre entreprise. C'est pourquoi nous nous engageons à utiliser des produits respectueux de l'environnement et à promouvoir des pratiques durables dans tous les aspects de notre activité, de la sélection des fournisseurs à la gestion des déchets."
-			/>
+			<div className="flex flex-col gap-8 lg:flex-row">
+				<Card
+					src={coffeIcon}
+					alt="coffe-icon"
+					title="Passioné de Café"
+					description="Notre équipe est passionnée par l'art de préparer le café. Chaque grain est soigneusement sélectionné pour offrir une expérience gustative exceptionnelle. Nous nous engageons à vous offrir des saveurs riches et uniques dans chaque tasse."
+				/>
+				<Card
+					src={catIcon}
+					alt="cat-icon"
+					title="Bien-être des Chats"
+					description="Le bonheur et le bien-être de nos amis félins sont au cœur de notre philosophie. Nous offrons un environnement sûr et confortable à nos chats résidents, où ils peuvent interagir avec nos clients et recevoir toute l'attention et l'affection qu'ils méritent."
+				/>
+				<Card
+					src={plantIcon}
+					alt="plant-icon"
+					title="Engagement Environnemental"
+					description="Nous sommes soucieux de l'impact environnemental de notre entreprise. C'est pourquoi nous nous engageons à utiliser des produits respectueux de l'environnement et à promouvoir des pratiques durables dans tous les aspects de notre activité, de la sélection des fournisseurs à la gestion des déchets."
+				/>
+			</div>
 		</section>
 	)
 }
