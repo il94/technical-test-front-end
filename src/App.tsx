06f839e22d9f705e6c67c54cdd09afeac7f5deed
il94 <ilyes.landolsi@outlook.fr>
2025-04-30 18:50:09 +0200

    feat: add AboutSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import HeroSection from "./views/HeroSection"
+import AboutSection from "./views/HeroSection/AboutSection"
+
 function App() {
 	return (
 		<main>
 			<HeroSection />
+			<AboutSection />
 		</main>
 	)
 }
