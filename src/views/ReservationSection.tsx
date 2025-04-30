36120334cbe91d87b00d0bc9a7c0d3a4164fe8ba
il94 <ilyes.landolsi@outlook.fr>
2025-04-30 20:59:25 +0200

    update: add desktop display of ReservationSection

--- src/views/ReservationSection.tsx
+++ src/views/ReservationSection.tsx
@@ -1,16 +1,18 @@
 import { FormEvent, useCallback } from "react"
 import Button from "../components/Button"
-
+import { cn } from "../lib/utils"
 type ReservationFieldProps = {
 	name: string
 	label: string
 	type?: string
+	className?: string
 }
 
 function ReservationField({
 	name,
 	label,
 	type = "text",
+	className,
 }: ReservationFieldProps) {
 	return (
 		<div className="w-full">
@@ -21,7 +23,7 @@ function ReservationField({
 				name={name}
 				type={type}
 				required
-				className="border-foreground w-full border-b-2"
+				className={cn("border-foreground w-full border-b-2", className)}
 			/>
 		</div>
 	)
@@ -36,12 +38,12 @@ export default function ReservationSection() {
 	}, [])
 
 	return (
-		<section className="bg-background-secondary flex w-full flex-col items-center justify-center gap-12 px-7 py-20">
-			<div className="flex flex-col gap-8">
-				<h2 className="font-nohemi text-center text-4xl font-semibold">
+		<section className="bg-background-secondary flex w-full flex-col items-center justify-center gap-8 px-7 py-20 lg:flex-row lg:gap-20 lg:px-20">
+			<div className="flex h-full flex-col gap-8 lg:w-6/7 lg:items-start lg:gap-14">
+				<h2 className="font-nohemi text-center text-4xl font-semibold lg:text-5xl">
 					Réservez votre table !
 				</h2>
-				<p className="leading-[180%]">
+				<p className="h-full leading-[180%] lg:text-xl">
 					Réservez dès maintenant votre table pour une expérience caféinée
 					inoubliable en compagnie de nos adorables chats. Plongez dans
 					l'ambiance apaisante de Café Chatouille et laissez-vous séduire par
@@ -52,13 +54,24 @@ export default function ReservationSection() {
 			</div>
 			<form
 				onSubmit={onSubmit}
-				className="flex w-full flex-col items-end gap-6"
+				className="flex w-full flex-col items-end gap-6 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:gap-y-8"
 			>
 				<ReservationField name="name" label="Nom" />
 				<ReservationField name="firstname" label="Prénom" />
-				<ReservationField name="tel" label="N° de téléphone" />
-				<ReservationField name="email" label="Adresse email" type="email" />
-				<Button>Réserver</Button>
+				<ReservationField
+					name="tel"
+					label="N° de téléphone"
+					className="w-full"
+				/>
+				<ReservationField
+					name="email"
+					label="Adresse email"
+					type="email"
+					className="w-full"
+				/>
+				<div className="flex w-full justify-end lg:col-span-2">
+					<Button>Réserver</Button>
+				</div>
 			</form>
 		</section>
 	)
